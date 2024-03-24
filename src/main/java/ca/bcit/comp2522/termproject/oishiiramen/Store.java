4a704fc1b60c0e0ebd07bca8ce1d73a74c97767e
Misuzu <me.tpn23@gmail.com>
2024-03-24 16:56:22 -0700

    Create constructor in Store.java

--- src/main/java/ca/bcit/comp2522/termproject/oishiiramen/Store.java
+++ src/main/java/ca/bcit/comp2522/termproject/oishiiramen/Store.java
@@ -3,11 +3,13 @@ package ca.bcit.comp2522.termproject.oishiiramen;
 import java.util.ArrayList;
 
 public class Store {
-    public static final double EVENT_GOOD_POSSIBILITY;
-    public static final double EVENT_BAD_POSSIBILITY;
-    public static final double DOWNTOWN_RENT;
-    public static final double RICHMOND_RENT;
-    public static final double METROTOWN_RENT;
+    public static final double EVENT_GOOD_POSSIBILITY = 0.05;
+    public static final double EVENT_BAD_POSSIBILITY = 0.04;
+    public static final double DOWNTOWN_RENT = 10000.00;
+    public static final double RICHMOND_RENT = 9000.00;
+    public static final double METROTOWN_RENT = 8000.00;
+    public static final int MINIMUM_NUMBER_OF_CHAIR = 5;
+    public static final int MAX_NUMBER_OF_CHAIR = 30;
 
     private double effectAmountGood;
     private double effectAmountBad;
@@ -25,7 +27,24 @@ public class Store {
     private double accumulatedSales;
 
     private Store(final String name, final String ownerName, final String location, final int numberOfChair) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("The store name cannot be null.");
+        } else {
+            this.name = name.strip().substring(0, 1).toUpperCase()
+                    + name.strip().substring(1).toLowerCase();
+        }
 
+        if (ownerName == null || ownerName.isBlank()) {
+            throw new IllegalArgumentException("The owner name cannot be null.");
+        } else {
+            this.ownerName = ownerName.strip().toLowerCase();
+        }
+
+        this.location = location;
+
+        if (numberOfChair < MINIMUM_NUMBER_OF_CHAIR) {
+            this.numberOfChair = MINIMUM_NUMBER_OF_CHAIR;
+        } else this.numberOfChair = Math.min(numberOfChair, MAX_NUMBER_OF_CHAIR);
     }
 
 }
