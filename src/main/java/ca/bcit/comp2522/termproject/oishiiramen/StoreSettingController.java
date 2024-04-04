c62375f51a129dbc15175b23ba49848a887348f8
Atsuko Uemura <atsukof.kt@gmail.com>
2024-04-04 14:58:27 -0700

    Add placeholders for storeSetting.fxml

--- src/main/java/ca/bcit/comp2522/termproject/oishiiramen/StoreSettingController.java
+++ src/main/java/ca/bcit/comp2522/termproject/oishiiramen/StoreSettingController.java
@@ -6,7 +6,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,13 +17,28 @@ public class StoreSettingController {
     private Stage stage;
     private Scene scene;
     private Parent root;
+
     @FXML
     TextField ownerNameTextField;
+
+    @FXML
+    TextField storeNameTextField;
+
+    @FXML
+    ToggleGroup locationToggle;
+
+    @FXML
+    TextField numberOfChairTextField;
 
     public void switchToHiringEmployee(ActionEvent event) throws IOException {
 //        root = FXMLLoader.load(getClass().getResource("hiringEmployee.fxml"));
 
         String username = ownerNameTextField.getText();
+        String storename = storeNameTextField.getText();
+
+        RadioButton selectedRadioButton = (RadioButton) locationToggle.getSelectedToggle();
+        String storeLocation = selectedRadioButton.getText();
+        System.out.println("Selected Option: " + storeLocation);
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("hiringEmployee.fxml"));
         root = loader.load();
