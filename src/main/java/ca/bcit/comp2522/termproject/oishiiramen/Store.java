d1217b46f4edfca74786c9ea15f5b865bd483e82
Misuzu <me.tpn23@gmail.com>
2024-04-06 13:26:20 -0700

    Create run business method in Store.java

--- src/main/java/ca/bcit/comp2522/termproject/oishiiramen/Store.java
+++ src/main/java/ca/bcit/comp2522/termproject/oishiiramen/Store.java
@@ -1,6 +1,7 @@
 package ca.bcit.comp2522.termproject.oishiiramen;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Store {
     public static final double EVENT_GOOD_POSSIBILITY = 0.05;
@@ -10,6 +11,7 @@ public class Store {
     public static final double METROTOWN_RENT = 8000.00;
     public static final int MINIMUM_NUMBER_OF_CHAIR = 5;
     public static final int MAX_NUMBER_OF_CHAIR = 30;
+    public static final int OPERATION_HOURS = 8;
 
     private double effectAmountGood;
     private double effectAmountBad;
@@ -19,13 +21,14 @@ public class Store {
     private final ArrayList<Employee> employees;
     private int numberOfChair;
     private double rent;
-    private int occupancyRateLower;
-    private int occupancyRateUpper;
+    private double occupancyRateLower;
+    private double occupancyRateUpper;
     private final ArrayList<Menu> menu;
 
     private double accumulatedCost;
     private double accumulatedSales;
 
+    Random random;
 
     public Store(final String name, final String ownerName, final Location location, final int numberOfChair) {
         if (name == null || name.isBlank()) {
@@ -49,10 +52,16 @@ public class Store {
 
         if (this.location.equals("Downtown")) {
             this.rent = DOWNTOWN_RENT;
+            this.occupancyRateLower = 0.8;
+            this.occupancyRateUpper = 1.4;
         } else if (this.location.equals("Richmond")) {
             this.rent = RICHMOND_RENT;
+            this.occupancyRateLower = 0.7;
+            this.occupancyRateUpper = 1.3;
         } else {
             this.rent = METROTOWN_RENT;
+            this.occupancyRateLower = 0.65;
+            this.occupancyRateUpper = 1.2;
         }
 
         if (numberOfChair < MINIMUM_NUMBER_OF_CHAIR) {
@@ -61,6 +70,11 @@ public class Store {
 
         this.menu = new ArrayList<>();
         this.employees = new ArrayList<>();
+
+        this.accumulatedCost = 0;
+        this.accumulatedSales = 0;
+
+        random = new Random();
     }
 
     public enum Location {
@@ -101,6 +115,14 @@ public class Store {
 //        pass;
 //    }
 
+    public double getAccumulatedCost() {
+        return this.accumulatedCost;
+    }
+
+    public double getAccumulatedSales() {
+        return this.accumulatedSales;
+    }
+
     public void setName(String newName) {
         if (newName == null || newName.isBlank()) {
             throw new IllegalArgumentException("The store name cannot be null.");
@@ -120,10 +142,16 @@ public class Store {
         // the rent will be changed as the location is changed
         if (this.location.equals("Downtown")) {
             this.rent = DOWNTOWN_RENT;
+            this.occupancyRateLower = 0.8;
+            this.occupancyRateUpper = 1.4;
         } else if (this.location.equals("Richmond")) {
             this.rent = RICHMOND_RENT;
+            this.occupancyRateLower = 0.7;
+            this.occupancyRateUpper = 1.3;
         } else {
             this.rent = METROTOWN_RENT;
+            this.occupancyRateLower = 0.65;
+            this.occupancyRateUpper = 1.2;
         }
     }
 
@@ -173,5 +201,71 @@ public class Store {
             }
         }
         return false;
+    }
+
+
+    private double calculateLabourCost(int days) {
+        // calculate labour cost and sum it up
+        double labourCost = 0.0;
+        for (Employee employee : employees) {
+            labourCost += employee.getHourlyWage() * OPERATION_HOURS * days;
+        }
+        return labourCost;
+    }
+
+    public double runBusiness(int days) {
+        // calculate the sales and cost during the days
+        double sales = 0.0;
+        double cost = 0.0;
+
+        cost += calculateLabourCost(days);
+
+        // calculate rent
+        if (location.equals("Downtown")) {
+            cost += DOWNTOWN_RENT * days / 30;
+        } else if (location.equals("Richmond")) {
+            cost += RICHMOND_RENT * days / 30;
+        } else {
+            cost += METROTOWN_RENT * days / 30;
+        }
+
+        // 頼まれやすさ = orderRate
+        double orderRate = 0.0;
+        int numberOfMenu = menu.size();
+        double sumOfPrice = 0;
+        for (Menu ramen: menu ) {
+            orderRate += 1/ramen.getPrice();
+            sumOfPrice+= ramen.getPrice();
+        }
+        double averagePrice = sumOfPrice/numberOfMenu;
+
+        //　set coefficient based on Average price
+        double customerCoefficient = 1.0;
+        if (averagePrice > 30) {
+            customerCoefficient = 0.7;
+        } else if (averagePrice > 25) {
+            customerCoefficient = 0.8;
+        }
+
+        // calculate by days user want to run business for
+        for (int i = 0; i < days; i++) {
+            double occupancy = random.nextDouble(occupancyRateLower, occupancyRateUpper);
+            // number of customers in a day
+            int numberOfCustomers = (int) (occupancy * numberOfChair * OPERATION_HOURS * customerCoefficient);
+
+            // 商品の価格平均によって稼働率を調整する
+
+            // 各商品の頼まれやすさの割合＝頼まれやすさ / 頼まれやすさの合計
+            // 頼まれやすさの合計は、「1/各メニューの価格」　の総和
+
+            for (Menu ramen: menu) {
+                sales += numberOfCustomers * ramen.getPrice() * ( 1 / ramen.getPrice() / orderRate);
+                cost += numberOfCustomers * ramen.getMaterialCost() * ( 1 / ramen.getPrice() / orderRate);
+            }
+        }
+
+        this.accumulatedCost += cost;
+        this.accumulatedSales += sales;
+        return sales-cost;
     }
 }
