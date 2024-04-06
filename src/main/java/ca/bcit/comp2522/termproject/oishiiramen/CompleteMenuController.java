f93647d9f3ac7e518ff08ef5db6fcef7e7472d7b
Misuzu <me.tpn23@gmail.com>
2024-04-06 15:29:47 -0700

    Fix bug to switch storeHome

--- src/main/java/ca/bcit/comp2522/termproject/oishiiramen/CompleteMenuController.java
+++ src/main/java/ca/bcit/comp2522/termproject/oishiiramen/CompleteMenuController.java
@@ -1,10 +1,13 @@
 package ca.bcit.comp2522.termproject.oishiiramen;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,21 +19,33 @@ public class CompleteMenuController {
     private Scene scene;
     private Parent root;
 
+    @FXML
+    ToggleGroup addMenuToggle;
+
     public void switchStoreHome(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("storeHome.fxml"));
-        root = loader.load();
+
+
+        RadioButton selectedAddMenuRadioButton = (RadioButton) addMenuToggle.getSelectedToggle();
+        String userWantsToAdd = selectedAddMenuRadioButton.getText();
+
+        if (userWantsToAdd.equals("Yes")) {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("ramenMenu1.fxml"));
+            root = loader.load();
+        } else {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("storeHome.fxml"));
+            root = loader.load();
+
+            // display store name
+            StoreHomeController storeHomeController = loader.getController();
+            storeHomeController.displayStoreName(HelloApplication.yourStore.getName());
+
+            // display store menu
+            storeHomeController.displayStoreMenu(HelloApplication.yourStore);
+        }
 
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
-
-        // display store name
-        StoreHomeController storeHomeController = loader.getController();
-        storeHomeController.displayStoreName(HelloApplication.yourStore.getName());
-
-        // display store menu
-        storeHomeController.displayStoreMenu(HelloApplication.yourStore);
-
     }
 }
