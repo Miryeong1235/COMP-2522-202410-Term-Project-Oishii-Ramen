07a38ce686432351d5f97017e338a63d0330cfd8
Atsuko Uemura <atsukof.kt@gmail.com>
2024-04-08 17:07:23 -0700

    Add javadoc comments on Menu.java

--- src/main/java/ca/bcit/comp2522/termproject/oishiiramen/Menu.java
+++ src/main/java/ca/bcit/comp2522/termproject/oishiiramen/Menu.java
@@ -9,22 +9,76 @@ import java.util.ArrayList;
  * @version 12-April-2024
  */
 public abstract class Menu {
+
+    /**
+     * Material cost for chashu.
+     */
     public static final double COST_FOR_CHASHU = 0.5;
+
+    /**
+     * Material cost for corn.
+     */
     public static final double COST_FOR_CORN = 0.1;
+
+    /**
+     * Material cost for tamago.
+     */
     public static final double COST_FOR_TAMAGO = 0.3;
+
+    /**
+     * Material cost for seaweed.
+     */
     public static final double COST_FOR_SEAWEED = 0.1;
 
+    /**
+     * Coefficient for large sized ramen.
+     */
     public static final double LARGE_SIZE_COEFFICIENT = 1.3;
+
+    /**
+     * The number of menu.
+     */
+    private static int numberOfMenu = 0;
+
+    /**
+     * Material cost for the menu object.
+     */
     protected double costForRamen;
+
+    /**
+     * Menu ID for the menu.
+     */
     protected int menuID;
+
+    /**
+     * Name for the menu.
+     */
     protected String name;
+
+    /**
+     * An ArrayList of the toppings.
+     */
     protected ArrayList<Topping> toppings;
+
+    /**
+     * Size for the menu.
+     */
     protected final String size;
+
+    /**
+     * Price for the menu.
+     */
     protected double price;
+
+    /**
+     * Material cost for the menu.
+     */
     protected double materialCost;
 
-    static int numberOfMenu = 0;
-
+    /**
+     * The Topping enum represents the possible toppings which a menu object can have.
+     * It provides four options: chashu, corn, tamago, and seaweed.
+     */
     public enum Topping {
         chashu,
         corn,
@@ -32,10 +86,19 @@ public abstract class Menu {
         seaweed,
     }
 
+    /**
+     * The RamenSize enum represents the possible size of the ramen.
+     * It provides three options: large and medium.
+     */
     public enum RamenSize {
         large, medium
     }
 
+    /**
+     * Constructs a Menu object with a parameter.
+     *
+     * @param size as a String
+     */
     public Menu(String size) {
         numberOfMenu++;
         this.menuID = numberOfMenu;
@@ -53,37 +116,78 @@ public abstract class Menu {
         this.name = this.getClass().getName() + this.getSize();
     }
 
+    /**
+     * Returns a cost for the menu.
+     *
+     * @return costForRamen as a double
+     */
     public double getCostForRamen() {
         return costForRamen;
     }
 
+    /**
+     * Returns menuID.
+     *
+     * @return menuID as an int
+     */
     public int getMenuID() {
         return menuID;
     }
 
+    /**
+     * Returns name.
+     *
+     * @return name as a String
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns the ArrayList of Topping.
+     *
+     * @return toppings as an ArrayList
+     */
     public ArrayList<Topping> getToppings() {
         return toppings;
     }
 
+    /**
+     * Returns the size for the menu.
+     *
+     * @return size as a String
+     */
     public String getSize() {
         return size;
     }
 
+    /**
+     * Returns a price for the menu.
+     *
+     * @return price as a double
+     */
     public double getPrice() {
         return price;
     }
 
+    /**
+     * Returns a material cost for the menu.
+     *
+     * @return materialCost as a double
+     */
     public double getMaterialCost() {
         return materialCost;
     }
 
-    public void setName(String newName) {
+    /**
+     * Sets name.
+     *
+     * @param newName as a String
+     */
+    public void setName(final String newName) {
         this.name = newName;
     }
+
 
     //今のままだとtoppingを一度addしたら変えられないかも
     public void setToppings(ArrayList<Topping> toppings) {
@@ -110,5 +214,9 @@ public abstract class Menu {
 
     public void setPrice(double newPrice) {
         this.price = newPrice; // Add some logic
+    }
+
+    public static int getNumberOfMenu() {
+        return numberOfMenu;
     }
 }
