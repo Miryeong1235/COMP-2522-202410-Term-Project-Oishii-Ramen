64467b18f704dbecce07bb912c4ce21535b5451f
Misuzu <me.tpn23@gmail.com>
2024-04-09 21:46:25 -0700

    Add Javadoc in GameApplication.java

--- src/main/java/ca/bcit/comp2522/termproject/oishiiramen/GameApplication.java
+++ src/main/java/ca/bcit/comp2522/termproject/oishiiramen/GameApplication.java
@@ -16,8 +16,15 @@ import java.io.IOException;
 public class GameApplication extends Application {
 
     public static Store yourStore;
+
+    /**
+     * Starts the application by loading the welcome screen.
+     *
+     * @param stage the primary stage of the application
+     * @throws IOException if an error occurs while loading the FXML file
+     */
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(final Stage stage) throws IOException {
 
         FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("welcome.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
@@ -26,12 +33,27 @@ public class GameApplication extends Application {
         stage.show();
     }
 
-    public static void main(String[] args) {
+    /**
+     * Drive the program.
+     * @param args unused
+     */
+    public static void main(final String[] args) {
         launch();
     }
 
-    public static Store initializeStore(String storename, String username, Store.Location storeLocation, int numberOfChair) {
-        yourStore = new Store(storename, username, storeLocation, numberOfChair);
+    /**
+     * Initializes a new store with the provided details.
+     *
+     * @param storeName      the name of the Store
+     * @param username       the owner name
+     * @param storeLocation  the location of the Store (Downtown, Richmond, or Metrotown)
+     * @param numberOfChair  the number of chairs in the Store
+     * @return the initialized Store
+     * @throws IllegalArgumentException if the location is invalid
+     */
+    public static Store initializeStore(final String storeName, final String username,
+                                        final Store.Location storeLocation, final int numberOfChair) {
+        yourStore = new Store(storeName, username, storeLocation, numberOfChair);
         return yourStore;
     }
 
