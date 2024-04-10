809dbf983bf32003912777206cee358760d34f58
Atsuko Uemura <atsukof.kt@gmail.com>
2024-04-09 23:21:20 -0700

    Remove unnecessary lines in WelcomeController.java

--- src/main/java/ca/bcit/comp2522/termproject/oishiiramen/WelcomeController.java
+++ src/main/java/ca/bcit/comp2522/termproject/oishiiramen/WelcomeController.java
@@ -1,16 +1,12 @@
 package ca.bcit.comp2522.termproject.oishiiramen;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
-
 import java.io.IOException;
-import java.util.Objects;
 
 /**
  * Welcome controller.
@@ -19,9 +15,6 @@ import java.util.Objects;
  * @version 12-April-2024
  */
 public class WelcomeController {
-    @FXML
-    private Label welcomeText;
-
     /**
      * Moves to store setting page on our JavaFX UI.
      *
