796cf123a52f834dc74f4769576319db21aa2cb6
Misuzu <me.tpn23@gmail.com>
2024-04-10 08:34:21 -0700

    Add unit tests to EmployeeTest.java

--- test/EmployeeTest.java
+++ test/EmployeeTest.java
@@ -28,81 +28,69 @@ class EmployeeTest {
     }
 
     @Test
-    public void multiParamGuppyIsSetToCorrectValues() {
-        assertEquals("Poecilia", testGuppy.getGenus());
-        assertEquals("reticulata", testGuppy.getSpecies());
-        assertEquals(1, testGuppy.getAgeInWeeks());
-        assertTrue(testGuppy.getIsFemale());
-        assertEquals(3, testGuppy.getGenerationNumber());
-        assertTrue(testGuppy.getIsAlive());
-        assertEquals(0.75, testGuppy.getHealthCoefficient(), 0.0);
+    public void employeeIsSetToCorrectValues() {
+        assertEquals("Taro", testEmployee.getName());
+        assertEquals(1, testEmployee.getLevel());
+        assertEquals(20, testEmployee.getHourlyWage());
+        assertEquals(1, testEmployee.getEmployeeID());
     }
 
     @Test
     public void identificationNumbersAreSequentialAndUniqueInMultiParamConstruction() {
-        Guppy first = new Guppy();
-        int firstID = first.getIdentificationNumber() + 1;
+        Employee first = new Employee("Ichiro", 1);
+        int firstID = first.getEmployeeID() + 1;
         int numberCreated = 100;
         for (int i = 0; i < numberCreated; ++i) {
-            Guppy fry = new Guppy("Poecilia",
-                    "reticulata",
-                    1,
-                    true,
-                    3,
-                    0.75);
-            assertEquals(firstID + i, fry.getIdentificationNumber());
+            Employee employee = new Employee("Shohei", 3);
+            assertEquals(firstID + i, employee.getEmployeeID());
         }
     }
 
     @Test
     public void genusAndSpeciesAreCorrectlyFormattedAndStored() {
-        Guppy fry = new Guppy("  poECILIA    ",
-                "  retICUlata   ",
-                1,
-                true,
-                3,
-                0.75);
-        assertEquals("Poecilia", fry.getGenus());
-        assertEquals("reticulata", fry.getSpecies());
+        Employee newEmployee = new Employee("  jiro    ", 5
+);
+        assertEquals("Jiro", newEmployee.getName());
+        assertEquals(3, newEmployee.getLevel());
     }
 
-    @Test
-    public void nullSpeciesThrowsIllegalArgumentException() {
-        // check if default value is used when null
-        assertThrows(IllegalArgumentException.class, () -> new Guppy("a", null, 0, true, 0 , 0.5));
-    }
-
-    @Test
-    public void negativeAgeInWeeksBecomesZero() {
-        // the level is not valid (0, 4)
-        Guppy fry = new Guppy("a",
-                "b",
-                -1,
-                true,
-                0 ,
-                0.5);
-        assertEquals(0, fry.getAgeInWeeks());
-    }
-
-
-
-    public void setName(final String newName) {
-        this.name = newName;
-    }
-    // null, valid, blank
-
-    public void setLevel(final int newLevel) {
-        if (newLevel >= MIN_LEVEL && newLevel <= MAX_LEVEL) {
-            this.level = newLevel;
-        } else if (newLevel < MIN_LEVEL) {
-            this.level = MIN_LEVEL;
-        } else {
-            this.level = MAX_LEVEL;
-        }
-        setHourlyWage();
-    }
-    // valid value, 0, 4
-
-    // toString test
+//    @Test
+//    public void nullSpeciesThrowsIllegalArgumentException() {
+//        // check if default value is used when null
+//        assertThrows(IllegalArgumentException.class, () -> new Guppy("a", null, 0, true, 0 , 0.5));
+//    }
+//
+//    @Test
+//    public void negativeAgeInWeeksBecomesZero() {
+//        // the level is not valid (0, 4)
+//        Guppy fry = new Guppy("a",
+//                "b",
+//                -1,
+//                true,
+//                0 ,
+//                0.5);
+//        assertEquals(0, fry.getAgeInWeeks());
+//    }
+//
+//
+//
+//    public void setName(final String newName) {
+//        this.name = newName;
+//    }
+//    // null, valid, blank
+//
+//    public void setLevel(final int newLevel) {
+//        if (newLevel >= MIN_LEVEL && newLevel <= MAX_LEVEL) {
+//            this.level = newLevel;
+//        } else if (newLevel < MIN_LEVEL) {
+//            this.level = MIN_LEVEL;
+//        } else {
+//            this.level = MAX_LEVEL;
+//        }
+//        setHourlyWage();
+//    }
+//    // valid value, 0, 4
+//
+//    // toString test
 
 }
