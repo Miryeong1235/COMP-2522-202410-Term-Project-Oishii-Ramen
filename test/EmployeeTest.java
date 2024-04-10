937c3809ed9dae8f0dfd610d2d85979f194d49f1
Misuzu <me.tpn23@gmail.com>
2024-04-09 23:43:58 -0700

    Add test files for ramen

--- test/EmployeeTest.java
+++ test/EmployeeTest.java
@@ -1,3 +1,16 @@
-class EmployeeTest {
+import ca.bcit.comp2522.termproject.oishiiramen.Employee;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+class EmployeeTest {
+    @Test
+    public void zeroParamGuppyIsSetToCorrectValues() {
+        Employee employee = new Employee("John", 2);
+        assertEquals(1, employee.getEmployeeID());
+        assertEquals("John", employee.getName());
+        assertEquals(2, employee.getLevel());
+        assertEquals(25.0, employee.getHourlyWage());
+    }
 }
