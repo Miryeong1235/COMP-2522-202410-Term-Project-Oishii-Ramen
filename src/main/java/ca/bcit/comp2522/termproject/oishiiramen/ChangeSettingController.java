acc28322361775804a495b67a670f8a426399ea6
Misuzu <me.tpn23@gmail.com>
2024-04-12 15:31:46 -0700

    Add backToStoreHome method

--- src/main/java/ca/bcit/comp2522/termproject/oishiiramen/ChangeSettingController.java
+++ src/main/java/ca/bcit/comp2522/termproject/oishiiramen/ChangeSettingController.java
@@ -1,4 +1,39 @@
 package ca.bcit.comp2522.termproject.oishiiramen;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * Change setting controller.
+ *
+ * @author Atsuko Uemura, Misuzu Taniguchi
+ * @version 12-April-2024
+ */
 public class ChangeSettingController {
+    /**
+     * Moves to store home page on our JavaFX UI.
+     *
+     * @param event as ActionEvent
+     * @throws IOException when I/O operation is failed.
+     */
+    public void backToStoreHome(final ActionEvent event) throws IOException {
+        Stage stage;
+        Scene scene;
+        Parent root;
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("storeHome.fxml"));
+        root = loader.load();
+
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
